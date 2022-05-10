3760c0134977b84c170b9f54b9fca430d1fa7a99
Hikmahx <hikmayousuph@gmail.com>
2022-05-09 22:21:04 -1100

    routes added

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ connectDB();
 
 const app = express()
 
-// app.use(express.json({ extended: false })); 
+app.use(express.json({ extended: false })); 
 
 const PORT = 5000 || process.env.PORT
+
+
+// ROUTES
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/users', require('./routes/users'))
 
 app.listen(PORT, ()=> console.log('This is listening on PORT: ' + PORT))
